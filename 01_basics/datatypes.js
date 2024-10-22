e1bf7c1784d2a0be2556d93fe26891aa4bbc8270
Developer5050 <166867782+Developer5050@users.noreply.github.com>
2024-10-22 17:07:47 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all Js code as newer version
+
+let name = "ahmad"
+let age = 22
+let isLoggedIn = true/ false 
+
+// original documnetation .... tc39.es
+
+// primitive datatypes
+
+// string => double quote
+// number => 2 the power 53
+// bigint
+// boolen => true / false
+// symbol => unique
+// null => standlone value 
+// undefined
+// object 
+
+console.log(typeof "ali");
+console.log(typeof age);
+console.log(typeof null); // object 
+console.log(typeof undefined); // undefined
+
+
